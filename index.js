76b44de393e63f392b37f7ed21a795f6c5ccca08
Vincent Danchaud <vdanchaud@gmail.com>
2026-01-14 13:03:33 +0100

    update copy to clipboard function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const form = document.getElementById('color-form');
+const darkmodeValue = document.getElementById('darkmode').checked;
 
+initHTML();
+
+// ------ EVENT LISTENERS -------
 document.getElementById('submit-btn').addEventListener('click', (e) => {
   e.preventDefault();
 
@@ -13,6 +17,53 @@ document.getElementById('submit-btn').addEventListener('click', (e) => {
   getColorScheme(colorHex, schemeMode);
 });
 
+document.getElementById('darkmode').addEventListener('click', () => {
+  if (document.getElementById('darkmode').checked) {
+    document.body.classList.add('darkmode');
+  } else {
+    document.body.classList.remove('darkmode');
+  }
+});
+
+// ------ FUNCTIONS -------
+function initHTML() {
+  const formData = new FormData(form);
+  const colorHex = formData.get('color').indexOf('#') != -1 ? formData.get('color').slice(1) : formData.get('color');
+  const schemeMode = formData.get('scheme');
+
+  getColorScheme(colorHex, schemeMode);
+}
+
+function renderColors(colorsArr) {
+  let html = '';
+
+  for (const color of colorsArr) {
+    html += `
+        <div class="color-container">
+            <img src="${color.image.bare}" alt="${color.name.value}" class="color-img" />
+            <div class="color-text-container">
+                <div class="color-copy-container">
+                  <button class="copy-btn" onClick="copyToClipboard(event)" data-hex="${color.hex.value}">
+                    <i class="fa-solid fa-copy" data-hex="${color.hex.value}"></i>
+                  </button>
+                  <p class="color-text">${color.hex.value}</p>
+                </div>
+                <p class="color-name-text">${color.name.value}</p>
+            </div>
+        </div>  
+    `;
+  }
+
+  document.getElementById('colors').innerHTML = html;
+}
+
+function copyToClipboard(e) {
+  if (e.target.dataset) {
+    const hexValue = e.target.dataset.hex;
+    navigator.clipboard.writeText(hexValue);
+  }
+}
+
 async function getColor(colorHex) {
   const response = await fetch(`https://www.thecolorapi.com/id?format=json&hex=${colorHex}`);
   const data = await response.json();
@@ -21,21 +72,7 @@ async function getColor(colorHex) {
 async function getColorScheme(colorHex, schemeMode) {
   const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${schemeMode}&format=json`);
   const data = await response.json();
-  const colors = data.colors;
-  console.log(colors);
-  for (const color of colors) {
-    document.getElementById('colors').innerHTML += `
-        <div class="color-container">
-            <img src="${color.image.bare}" alt="${color.name.value}" class="color-img" />
-            <div class="color-text-container">
-                <div class="color-copy-container">
-                  <i class="fa-solid fa-copy"></i>
-                  <p class="color-text">${color.hex.value}</p>
-                </div>
-                <p class="color-name-text">${color.name.value}</p>
-            </div>
-        </div>
-        
-    `;
-  }
+  const colorsArray = data.colors;
+
+  renderColors(colorsArray);
 }
